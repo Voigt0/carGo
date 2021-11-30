478f38789d56bd5e1eef217abf6b8e978e243ee4
Voigt0 <voigtrodrigo0@gmail.com>
2021-11-30 09:59:47 -0300

    Create compra.php

---
+++ php/compra.php
@@ -0,0 +1,21 @@
+<?php include 'nav.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>carGO!</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href=''>
+    <script src=''></script>
+</head>
+<body>
+    <content>
+        <div id="texto">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut faucibus massa. Aenean at tortor lacinia, blandit felis sit amet, placerat eros. Proin ullamcorper orci et lectus fermentum luctus. Vivamus quis porttitor massa, nec tincidunt eros. Morbi vel quam convallis dui fermentum pellentesque in ac nisi. Mauris gravida iaculis massa non sodales. Aliquam lorem nisi, vulputate sit amet tristique ac, condimentum sed dolor. Aliquam fermentum massa sed diam convallis, nec blandit urna fringilla. Integer ac mi quis ipsum mattis sagittis quis non eros. Fusce gravida augue sed nunc vulputate suscipit. Pellentesque nulla odio, porttitor id rhoncus id, congue eu orci. Aenean consequat porta ipsum, et placerat tellus fringilla vel. Cras dapibus, ligula ut auctor porta, sem sem tincidunt erat, eget lobortis ligula nibh vitae eros. In vel rhoncus nunc. Aenean ut viverra nibh. Fusce interdum nunc id dictum facilisis.
+
+            Curabitur viverra nibh enim, at imperdiet dolor pellentesque sit amet. Proin convallis tempor tortor quis euismod. Nullam facilisis mi ac turpis congue, non mollis sem iaculis. Integer pharetra quis enim a aliquam. Vivamus ut ipsum et libero consequat lacinia. Donec posuere rhoncus lorem, quis lobortis neque vulputate vitae. Sed varius dui ac enim lobortis, id consectetur purus porttitor. Curabitur orci lectus, dapibus non malesuada at, scelerisque sit amet urna. Fusce laoreet neque ut erat pulvinar, in ultricies felis tristique. Nunc tempus non libero ac bibendum. Nulla a sollicitudin nisi.
+        </div>
+    </content>
+</body>
+</html>
